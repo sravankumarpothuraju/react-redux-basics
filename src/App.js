615b05ae19e62e1b741438992537e77cd79fd7c1
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-12-29 15:38:51 -0500

    created store and providing the store to the react application with the help of provider from react-redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
 
 import CakeContainer from "./components/CakeContainer";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <div>
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div>
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 };
 
