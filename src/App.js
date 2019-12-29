44af56bdbacecf3e8215734accca1e1a4f5e2da9
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-12-29 15:27:18 -0500

    added reducers

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CakeContainer from "./components/CakeContainer";
+
+const App = () => {
+  return (
+    <div>
+      <CakeContainer />
+    </div>
+  );
+};
+
+export default App;
