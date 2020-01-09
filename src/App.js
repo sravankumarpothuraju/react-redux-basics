be00c377b36836358d4628025a4b4b6dc2064811
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-08 23:58:38 -0500

    added mapstatetoprops with ownprops usage

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import CakeContainer from "./components/CakeContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 import store from "./redux/store";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
+        <ItemContainer />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
